99917de1a8559ce45f72084d8cb71ead9b28ab4f
LiLuo-B <474115040@qq.com>
2024-06-12 11:03:56 +0800

    id提取函数添加

--- nonebot_plugin_mcsm/__main__.py
+++ nonebot_plugin_mcsm/__main__.py
@@ -10,8 +10,15 @@ from nonebot.adapters.onebot.v11 import (
     Bot,
 )
 from nonebot.adapters.onebot.v11.message import Message
-from .mcsm_api import get_node_list, get_instance_list
-from .utils import get_index
+from .mcsm_api import (
+    get_node_list,
+    get_instance_list,
+    start_instance,
+    stop_instance,
+    kill_instance,
+    restart_instance,
+)
+from .utils import get_index, get_indexs
 
 show_node_list = on_command("节点列表", permission=SUPERUSER)
 show_instance_list = on_command("实例列表", permission=SUPERUSER)
@@ -76,3 +83,27 @@ async def _(event: MessageEvent):
                 await show_instance_list.finish()
         await show_instance_list.finish("没查到该ID对应的节点")
     await show_instance_list.finish("参数错误，已退出流程")
+
+
+@start_instance.handle()
+async def _(args: Message = CommandArg()):
+    if args := args.extract_plain_text():
+        index = get_index(args)
+        if index != None:
+            nodes = await get_node_list()
+            if isinstance(nodes, int):
+                await show_instance_list.finish(f"节点查询失败，错误码{nodes}")
+            for node in nodes:
+                if index == node.index:
+                    instances = await get_instance_list(node.daemon_id)
+                    if isinstance(instances, int):
+                        await show_instance_list.finish(
+                            f"实例查询失败，错误码{instances}"
+                        )
+                    for instance in instances:
+                        await show_instance_list.send(
+                            f"序号：{instance.index} 备注：{instance.instance_name} 状态：{instance.instance_status} 启动命令：{instance.start_command} 停止命令：{instance.stop_command} 更新命令：{instance.update_command}"
+                        )
+                    await show_instance_list.finish()
+            await show_instance_list.finish("没查到该ID对应的节点")
+    await show_instance_list.pause("请输入节点ID")
--- nonebot_plugin_mcsm/utils.py
+++ nonebot_plugin_mcsm/utils.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, Tuple, Union
 
 
 def get_error_message(status_code: int) -> str:
@@ -11,9 +11,23 @@ def get_error_message(status_code: int) -> str:
     return "响应正常"
 
 
-# 将用户传递参数的提取第一个并判断是否为id
+# 将用户传递的参数提取第一个并判断是否为id
 def get_index(arg: str) -> Optional[int]:
     arg = arg.split()[0]
     if arg.isdigit():
         return int(arg)
+    return None
+
+
+# 将用户传递的参数提取前两个并判断是否为id
+def get_indexs(arg: str) -> Union[None, int, Tuple[int, int]]:
+    arg = arg.split()
+    daemon_index = arg[0]
+    if daemon_index.isdigit():
+        if len(arg) == 2:
+            instance_index = arg[1]
+            if instance_index.isdigit():
+                return daemon_index, instance_index
+        else:
+            return daemon_index
     return None
