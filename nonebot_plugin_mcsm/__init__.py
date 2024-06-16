c7f1f95efbbb753c544ab559e2f8b17cb74b75f4
LiLuo-B <474115040@qq.com>
2024-06-17 00:59:24 +0800

    __plugin_meta__ 修改

--- nonebot_plugin_mcsm/__init__.py
+++ nonebot_plugin_mcsm/__init__.py
@@ -3,8 +3,8 @@ from . import __main__ as __main__
 from .config import Config
 
 __plugin_meta__ = PluginMetadata(
-    name="nonebot_plugin_mcsm",
-    description="NoneBot插件，对接MCSM，可通过机器人来管理实例",
+    name="MCSM小助手",
+    description="对接MCSM，可查看面板、节点、实例信息以及管理实例",
     usage="私聊或群里发送消息",
     type="application",
     homepage="https://github.com/LiLuo-B/nonebot-plugin-mcsm",
